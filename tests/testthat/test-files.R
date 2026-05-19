f4337db2b00debffef6534ba452664d2ae4602e0
Thomas Wilson [tpw2] (Staff) <tpw2@.aber.ac.uk>
2026-05-19 09:24:48 +0100

    Add regression tests for mzML parsing

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -1,24 +1,86 @@
+fixture_file <- function(path)
+{
+  local_path <- testthat::test_path("..", "..", "inst", "extdata", path)
 
-test_that('file versions', {
+  if (file.exists(local_path)) {
+    return(local_path)
+  }
 
+  system.file("extdata", path, package = "q3ML")
+}
 
-  testfiles <-
-    list.files(system.file('extdata', package = 'q3ML'), full.names = TRUE)
+write_fixture_variant <- function(path, edit_xml)
+{
+  xml <- xml2::read_xml(path)
+  edit_xml(xml)
 
-  expect_true(class(mzR::openMSfile(testfiles[1])) == 'mzRpwiz')
-  expect_error(mzR::openMSfile(testfiles[2]))
+  variant <- tempfile(fileext = ".mzML")
+  xml2::write_xml(xml, variant)
+  variant
+}
 
-  expect_null(openFile(testfiles[1]))
-  expect_true(is.list(openFile(testfiles[2])))
+test_that("file versions", {
+  old_fixture <- fixture_file("QC01_pwiz3_0_1.mzML")
+  new_fixture <- fixture_file("QC01_pwiz3_0_2.mzML")
 
-  mzR_pwiz3_1 <- mzR::openMSfile(testfiles[1])
-  mzR_pwiz3_1_pks <- mzR::chromatograms(mzR_pwiz3_1)
-  mzR_pwiz3_1_hd <- mzR::chromatogramHeader(mzR_pwiz3_1)
+  expect_true(file.exists(old_fixture))
+  expect_true(file.exists(new_fixture))
 
-  q3ML_pwiz3_2 <- openFile(testfiles[2])
+  old_mzr <- mzR::openMSfile(old_fixture)
+  on.exit(mzR::close(old_mzr), add = TRUE)
 
-  expect_identical(mzR_pwiz3_1_pks, q3ML_pwiz3_2$peaks)
-  expect_identical(mzR_pwiz3_1_hd, q3ML_pwiz3_2$header)
+  expect_true(inherits(old_mzr, "mzRpwiz"))
+  expect_null(openFile(old_fixture))
 
+  q3ml_result <- openFile(new_fixture)
+  expect_true(is.list(q3ml_result))
 
-  })
+  mzr_result <- mzR::openMSfile(new_fixture)
+  on.exit(mzR::close(mzr_result), add = TRUE)
+
+  expect_identical(mzR::chromatograms(mzr_result), q3ml_result$peaks)
+  expect_identical(mzR::chromatogramHeader(mzr_result), q3ml_result$header)
+})
+
+test_that("ProteoWizard version checks are semantic", {
+  future_fixture <- write_fixture_variant(
+    fixture_file("QC01_pwiz3_0_2.mzML"),
+    function(xml) {
+      pwiz <- xml2::xml_find_first(xml, "//d1:software[@id='pwiz']")
+      xml2::xml_set_attr(pwiz, "version", "4.0.00001")
+    }
+  )
+  on.exit(unlink(future_fixture), add = TRUE)
+
+  expect_true(is.list(openFile(future_fixture)))
+})
+
+test_that("chromatogram parsing is resilient to cvParam ordering", {
+  baseline_fixture <- fixture_file("QC01_pwiz3_0_2.mzML")
+  baseline <- openFile(baseline_fixture)
+
+  reordered_fixture <- write_fixture_variant(
+    baseline_fixture,
+    function(xml) {
+      chromatogram <- xml2::xml_find_all(xml, "//d1:chromatogram")[[2]]
+      positive_scan <- xml2::xml_find_first(
+        chromatogram,
+        "./d1:cvParam[@name='positive scan']"
+      )
+
+      xml2::xml_add_sibling(
+        positive_scan,
+        "before",
+        .value = xml2::read_xml(
+          "<cvParam cvRef='MS' accession='MS:1000235' name='total ion current chromatogram' value=''/>"
+        )
+      )
+    }
+  )
+  on.exit(unlink(reordered_fixture), add = TRUE)
+
+  reordered <- openFile(reordered_fixture)
+
+  expect_identical(reordered$peaks, baseline$peaks)
+  expect_identical(reordered$header, baseline$header)
+})
